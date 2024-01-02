aee86a79ea8a6dc5c1a842cd6f74048a7428d058
khaleel raweh <khaleelvisa@gmail.com>
2024-01-03 01:27:25 +0300

    kjfkjdkfj

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -143,20 +143,23 @@
     @if (count($adv_sliders) > 0)
         <div class="holder mt-3">
             {{-- <div class="container-fluid px-0"> --}}
-            <div class="page-content px-0" style="min-height: 100px !important;">
+            <div class="page-content px-0" style="min-height: 100px !important;" data-start-height="10"
+                data-start-mheight="10">
                 {{-- <div class="row bnr-grid no-gutters"> --}}
                 {{-- <div class="prd-grid prd-promo-carousel  data-to-show-3   js-prd-promo-carousel"> --}}
 
                 <div class="prd-grid no-gutters prd-promo-carousel  js-prd-promo-carousel  data-to-show-3 data-to-show-md-3 data-to-show-sm-3 data-to-show-xs-2"
-                    data-slick='{"slidesToShow": 3, "slidesToScroll": 2, "responsive": [{"breakpoint": 992,"settings": {"slidesToShow": 3, "slidesToScroll": 1}},{"breakpoint": 768,"settings": {"slidesToShow": 2, "slidesToScroll": 1}},{"breakpoint": 480,"settings": {"slidesToShow": 2, "slidesToScroll": 1}}]}'>
+                    data-slick='{"slidesToShow": 3, "slidesToScroll": 2, "responsive": [{"breakpoint": 992,"settings": {"slidesToShow": 3, "slidesToScroll": 1}},{"breakpoint": 768,"settings": {"slidesToShow": 2, "slidesToScroll": 1}},{"breakpoint": 480,"settings": {"slidesToShow": 2, "slidesToScroll": 1}}]}'
+                    data-start-height="10" data-start-mheight="10">
 
                     @forelse ($adv_sliders as $adv_slider)
                         {{-- <div class="col-12 col-sm-4"> --}}
                         {{-- <div class="prod-promo prd-promo--lg prd-has-loader "> --}}
-                        <div class="prd prd--style2 prd-labels--max prd-labels-shadow ">
+                        <div class="prd prd--style2 prd-labels--max prd-labels-shadow " data-start-height="10"
+                            data-start-mheight="10">
 
                             {{-- <div class="bnr-wrap d-flex align-items-center h-100 bnr-1586628920521-0 "> --}}
-                            <div class="bnr-wrap d-flex  h-100 bnr-1586628920521-0 ">
+                            <div class="bnr-wrap d-flex align-items-center h-100 bnr-1586628920521-0 ">
                                 <div class="bnr custom-caption  image-hover-scale image-hover-scale--slow bnr--middle bnr--center my_adv_custom_style"
                                     data-fontratio="5.9">
                                     <div class="bnr-img image-container" style="padding-bottom: 38.933%">
@@ -205,6 +208,7 @@
                     @empty
                     @endforelse
                 </div>
+
             </div>
         </div>
     @endif
