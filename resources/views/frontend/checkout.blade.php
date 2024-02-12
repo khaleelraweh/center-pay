92eee5839787387db8ae4e7c3c5e30f4f4a52163
khaleel raweh <khaleelvisa@gmail.com>
2024-02-13 02:28:23 +0300

    allow customer to go back using shopping link in checkout

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -9,6 +9,7 @@
             <div class="container">
                 <ul class="breadcrumbs">
                     <li><a href="{{ route('frontend.index') }}">{{ __('panel.main') }}</a></li>
+                    <li><a href="{{ route('frontend.cart') }}">{{ __('panel.f_shopping_cart') }}</a></li>
                     <li><span> {{ __('panel.f_complete_your_purchase') }} </span></li>
                 </ul>
             </div>
