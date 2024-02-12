3e6ec7af410887e526ac2c512a6223b3a74426d4
khaleel raweh <khaleelvisa@gmail.com>
2024-02-12 18:43:32 +0300

    all is good

--- app/Http/Livewire/Frontend/Cart/CartItemComponent.php
+++ app/Http/Livewire/Frontend/Cart/CartItemComponent.php
@@ -9,46 +9,46 @@ use Livewire\Component;
 class CartItemComponent extends Component
 {
     use LivewireAlert;
-    
+
     public $item;
     public $item_quantity = 1;
 
-    public function mount(){
+    public function mount()
+    {
 
         $this->item_quantity = Cart::instance('default')->get($this->item)->qty ?? 1;
-        
     }
 
-    public function decreaseQuantity($rowId){
+    public function decreaseQuantity($rowId)
+    {
 
-        if($this->item_quantity > 1 ){
+        if ($this->item_quantity > 1) {
             $this->item_quantity = $this->item_quantity - 1;
             Cart::instance('default')->update($rowId, $this->item_quantity);
             $this->emit('updateCart');
         }
+    }
 
-    } 
+    public function increaseQuantity($rowId)
+    {
 
-    public function increaseQuantity($rowId){
-
-        if($this->item_quantity > 0 ){
+        if ($this->item_quantity > 0) {
             $this->item_quantity = $this->item_quantity + 1;
             Cart::instance('default')->update($rowId, $this->item_quantity);
             $this->emit('updateCart');
         }
-
     }
 
-    public function removeFromCart($rowId){
-         $this->emit('removeFromCart' , $rowId );
+    public function removeFromCart($rowId)
+    {
+        $this->emit('removeFromCart', $rowId);
     }
 
 
     public function render()
     {
-        return view('livewire.frontend.cart.cart-item-component' , [
-            'cartItem' =>Cart::instance('default')->get($this->item)
+        return view('livewire.frontend.cart.cart-item-component', [
+            'cartItem' => Cart::instance('default')->get($this->item)
         ]);
     }
 }
-
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,8 +43,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+     --}}
     {{-- fontAwesome call offLine --}}
     <link rel="stylesheet" href="{{ asset('frontend/css/vendor/fontAwesome.min.css') }}">
 
